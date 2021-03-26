94ff237992fc63fe8c108ad2e811a7f6887fca86
MiguelStern <mialnie@inf.upv.es>
2021-03-26 20:17:57 +0100

    prueba 5

---
+++ Assets/prueba 5/ControlAnimaciones.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ControlAnimaciones : MonoBehaviour
+{
+    public Animator animator;
+    public string parametrosBoolean;
+    bool b = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        b = !b;
+        animator.SetBool(parametrosBoolean, b);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) animator.SetTrigger("destruir");
+    }
+}
