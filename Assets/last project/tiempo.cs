967d86e93cb7d94734b730fe8f1b62ce2ebf60d6
MiguelStern <mialnie@inf.upv.es>
2021-05-23 16:03:52 +0200

    Last Project

---
+++ Assets/last project/tiempo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiempo : MonoBehaviour
+{
+
+    float TargetTime;
+    private float StartTime;
+    public AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetTime = Time.time - StartTime;
+        if (TargetTime == 26.0f) {
+            music();
+        }
+    }
+
+    private void music() {
+        source.PlayOneShot(clip);
+        
+    }
+}
---
+++ Assets/last project/win.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class win : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string message = "Enorabuena, has encontrado el tesoro";
+    private bool truers = false;
+    
+    public TextMeshPro textos;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (truers) textos.SetText(message);
+               
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            truers = true;   
+        }
+    }
+    
+   
+}
